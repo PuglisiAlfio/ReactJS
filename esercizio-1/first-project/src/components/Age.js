157ce943106046b5ebbdfa7f128cd0a5afbaccb0
Alfio Puglisi <puglisi_algio@ciao.it>
2023-04-27 08:55:23 +0200

    modifica componente Welcome

--- esercizio-1/first-project/src/components/Age.js
+++ esercizio-1/first-project/src/components/Age.js
@@ -1,9 +1,6 @@
 function Age(props){
     return <div>
-            {props.age >= 18 && props.age <= 65
-            ? <p>Your age is {props.age}</p>
-            : <p>Insert a valid age, between 18 and 65</p>
-            }
+            {props.age}
         </div>
 }
 
--- esercizio-1/first-project/src/components/Welcome.js
+++ esercizio-1/first-project/src/components/Welcome.js
@@ -3,7 +3,11 @@ import Age from "./Age"
 function Welcome(props){
     return <div>
                 <p>Welcome {props.name}!</p>
-                <Age age = {17}/>
+                {props.age >= 18 && props.age <= 65
+                ? <p>Your age is {props.age}</p>
+                : <p>Insert a valid age, between 18 and 65</p>
+                }
+                <Age/>
             </div>
 }
 
