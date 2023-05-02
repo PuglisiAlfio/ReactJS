d411dd3285bf6ae344926aa104c37b4f289da885
Alfio Puglisi <puglisi_algio@ciao.it>
2023-05-02 12:26:42 +0200

    Events - ClickTracker

---
+++ esercizio-1/first-project/src/components/Counter.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+ const ClickCounter = () => {
+    const [clickTarget, setClickTarget] = useState();
+
+        const click = (event) => {
+            setClickTarget(event.target.textContent);
+        }
+
+        return <>
+            <button onClick={click}>First Button</button>
+            <button onClick={click}>Second Button</button>
+            <button onClick={click}>Third Button</button>
+            <h1>Last Button Clicked: {clickTarget}</h1>
+        </>
+}
+
+export default ClickCounter;
