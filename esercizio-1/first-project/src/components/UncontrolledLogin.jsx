87674f77829463dbf46c8ae7a455ca593b26e525
Alfio Puglisi <puglisi_algio@ciao.it>
2023-05-05 11:27:19 +0200

    Forms - Automatic focus

---
+++ esercizio-1/first-project/src/components/UncontrolledLogin.jsx
@@ -0,0 +1,27 @@
+const UncontrolledLogin = () => {
+
+    const handleChangeForm = (event) => {
+      event.preventDefault();
+  
+      const username = event.target.username.value;  //con la Classe "event.target.element.username.value"
+      const password = event.target.password.value;
+      const checkbox = event.target.checkbox.checked;
+  
+      console.log({
+        username, password, checkbox
+      })
+    }
+  
+    return <>
+            <h3>Uncontrolled Form</h3>
+            <form onSubmit={handleChangeForm}>
+              <input type="text" name="username" autoFocus/>
+              <input type="password" name = "password" />
+              <input type="checkbox" name = "checkbox"/>
+              <button type="submit">Login</button>
+            </form>
+              
+          </>
+  }
+  
+  export default UncontrolledLogin
