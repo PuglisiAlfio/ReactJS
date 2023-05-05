5c6f81747b0e6a665cf5f96b6e0f73da836d2f2a
Alfio Puglisi <puglisi_algio@ciao.it>
2023-05-05 11:09:36 +0200

    Forms - `UncontrolledLogin` component

--- esercizio-1/first-project/src/components/UncontrolledLogin.jsx
+++ esercizio-1/first-project/src/components/UncontrolledLogin.jsx
@@ -25,34 +25,3 @@ const UncontrolledLogin = () => {
   }
   
   export default UncontrolledLogin
-
-  
-// const Login = (props) => {
-
-//     const onLogin = (event) => {
-//         event.preventDefault();
-//     let values = {
-//         name: props.name,
-//         lastName: props.lastName,
-//         password: props.pass,
-//     }
-//     console.log(values)
-//     }
-
-//     const resetContent = (event) => {
-//         event.preventDefault();
-//         props.emptyName.current.value = '';
-//         props.emptyLastName.current.value = '';
-//         props.emptyPass.current.value = '';
-//     }
-
-    
-//         return(
-//             <>
-//                 <button disabled={!props.name || !props.pass ||!props.lastName} onClick={onLogin}>Login</button>
-//                 <button onClick={resetContent}>Reset Content</button>
-//             </>
-//         )
-// }
-
-// export default Login
