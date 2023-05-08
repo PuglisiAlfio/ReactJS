3f20dfbc9b986361302afebcc73fd8769b67b1da
Alfio Puglisi <puglisi_algio@ciao.it>
2023-05-08 12:38:30 +0200

    useState - `ClickCounter` as a function component

--- esercizio-1/first-project/src/components/Counter.jsx
+++ esercizio-1/first-project/src/components/Counter.jsx
@@ -4,9 +4,16 @@ import { useState } from "react"
 function Counter() {
 
     const [counter, setCount] = useState(0);
+
     const increase = () => {
         setCount(count => count + 1)
     }
+
+    const decrease = () => {
+        if (counter > 0) {
+            setCount(count => count - 1);
+        }
+      };
 
     
 
@@ -14,7 +21,8 @@ function Counter() {
 
     return <>
             <h2>Count: {counter}</h2>
-            <button onClick={increase}>Click</button>
+            <button onClick={increase}>Incrementa</button>
+            <button onClick={decrease}>Decrementa</button>
             </>
 }
 
