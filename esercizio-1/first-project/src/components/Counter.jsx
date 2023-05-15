8da32fd32638cccea5cd0eb001fc9390832cb7a0
Alfio Puglisi <puglisi_algio@ciao.it>
2023-05-15 12:31:58 +0200

    Counter con useCallback

---
+++ esercizio-1/first-project/src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import { useCounter } from "./useCounter"
+
+function Counter() {
+ 
+    const {counter, increment, decrement, reset} = useCounter()
+
+    
+    return <>
+            <h2>Count: {counter}</h2>
+            <button onClick={increment}>Incrementa</button>
+            <button onClick={decrement}>Decrementa</button>
+            <button onClick={reset}>Resetta</button>
+            </>
+}
+
+export default Counter
---
+++ esercizio-1/first-project/src/components/useCounter.jsx
@@ -0,0 +1,28 @@
+import { useCallback, useState } from "react";
+
+export function useCounter(){
+    const [counter, setCount] = useState(0);
+
+    const handleIncrease= useCallback( function handleIncrease () {
+        setCount(count => count + 1)
+        console.log('increase by useCallback')
+    }, [])
+
+    const handleDecrease = useCallback( function handleDecrease() {
+        if (counter > 0) {
+            setCount(count => count - 1);
+        }
+      },[counter]) 
+
+    const handleReset = useCallback(function handleReset(){
+        setCount(count => count = 0)
+    }, []) 
+
+    return {
+        counter: counter,
+        increment: handleIncrease,
+        decrement: handleDecrease,
+        reset: handleReset
+    }
+
+}
