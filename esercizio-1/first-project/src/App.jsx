40bfdf8b4b0e8e63e3ef82fdabaade05daada30c
Alfio Puglisi <puglisi_algio@ciao.it>
2023-05-19 11:34:16 +0200

    React Router - Add three Links

---
+++ esercizio-1/first-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { Link, Route, Routes } from "react-router-dom";
+import ShowGithubUser from "./components/ShowGithubUser";
+
+
+function App() {
+  return <div>
+          <Routes>
+            <Route path="user/:username" element={<ShowGithubUser/>}/>
+          </Routes>
+          <Link to='/user/PuglisiAlfio'>Go to User PuglisiAlfio</Link>
+          <br />
+          <Link to='/user/yangus98'>Go to User Giuseppe Bianconi</Link>
+          <br />
+          <Link to='/user/PuglisiAlfio'>Go to User PuglisiAlfio</Link>
+        </div>
+}
+
+export default App;
