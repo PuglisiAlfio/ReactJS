858bc5665680493f508560f00848572ba67467e5
Alfio Puglisi <puglisi_algio@ciao.it>
2023-05-19 17:08:17 +0200

    Es 42

--- esercizio-1/first-project/src/components/CarDetails.jsx
+++ esercizio-1/first-project/src/components/CarDetails.jsx
@@ -1,33 +1,30 @@
 import { useEffect, useRef } from "react";
 
 function CarDetails(props) {
-    const carDetails = useRef('');
-    const carName = useRef('');
-    const carModel = useRef('');
-    const carYear = useRef ('');
-    const carColor = useRef('');
+    const carDetails = useRef('')
+    const carName = useRef('')
+    const carModel = useRef('')
+    const carYear = useRef('')
+    const carColor = useRef('')
 
-    const onSubmitDetails = (event) => {
-        event.preventDefault();
-        console.log([carName.current.value, carModel.current.value, carYear.current.value, carColor.current.value])
-        carName.current.value = '';
-        carModel.current.value = '';
-        carYear.current.value = '';
-        carColor.current.value = '';
-        
+    const onSubmit = (e) => {
+      e.preventDefault()
+      console.log([carName.current.value, carModel.current.value, carYear.current.value, carColor.current.value])
     }
 
-    useEffect(()=> {carDetails.current.reset()}, [props.initialData])
+    useEffect(() => {carDetails.current.reset();}, [props.initialData])
 
-    return <>
-            <form ref={carDetails} onSubmit={onSubmitDetails}>
-                <input type="text" name="name" onChange={e => (e.target.value)} defaultValue={props.initialData.name}/>
-                <input type="text" name="model" onChange={e => (e.target.value)} defaultValue={props.initialData.model}/>
-                <input type="text" name="year" onChange={e => (e.target.value)} defaultValue={props.initialData.year}/>
-                <input type="text" name="color" onChange={e => (e.target.value)} defaultValue={props.initialData.color}/>
-                <button type="submit">Load</button>
-            </form>
-            </>
+    return(
+        <>
+        <form ref={carDetails}>
+          <input ref={carName} name="name" type="text" onChange={e => (e.target.value)} defaultValue={props.initialData.name}/>
+          <input ref={carModel} name="model" type="text" onChange={e => (e.target.value)} defaultValue={props.initialData.model}/>
+          <input ref={carYear} name="year" type="text" onChange={e => (e.target.value)} defaultValue={props.initialData.year}/>
+          <input ref={carColor} name="color" type="text" onChange={e => (e.target.value)} defaultValue={props.initialData.color}/>
+          <button type="submit" onClick={onSubmit}>Load</button>
+        </form>
+      </>
+    )
 }
 
 export default CarDetails;
