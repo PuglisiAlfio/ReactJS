ee02240a7218620743d7be8f4c6a07823bd586d1
Alfio Puglisi <puglisi_algio@ciao.it>
2023-05-20 17:08:36 +0200

    Simulazione checkpoint

--- esercizio-1/first-project/src/components/Fetcher.jsx
+++ esercizio-1/first-project/src/components/Fetcher.jsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
 
-function Fetcher() {
+function Fetcher(props) {
     const [data, setData] = useState('')
 
 useEffect(() => {
     const fetchData = async () => {
-        const response = await fetch('https://swapi.dev/api/people/1/')
+        const response = await fetch(`https://swapi.dev/api/people/${props.id}/`)
         const newData = await response.json();
         setData(newData);
         console.log(newData)
     }
     fetchData();
- }, [])
+ }, [props.id])
 
  return <>
         <p>{data.name} {data.height}</p>
